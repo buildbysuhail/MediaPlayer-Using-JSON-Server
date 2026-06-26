b61b35a5c140296bbad6c7f8a8fa30aed3e4364b
suhail polosys <suhailthenoble@gmail.com>
2026-06-26 15:53:33 +0530

    Header.jsx: Download button with aura displays only in Electron app

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,11 +15,19 @@ function Header() {
   <i className="fa-solid fa-cloud-arrow-up fa-2xl fa-bounce text-sky-700 ms-7" />
     <Link to={'/'}><a className="btn btn-ghost hover:outline-none outline-white rounded-xl text-2xl lg:text-3xl ms-5 font-extrabold text-purple-800 hover:bg-gradient-to-r hover:from-blue-800 hover:to-purple-600 hover:text-transparent hover:bg-clip-text">Media Player</a></Link>
   </div>
+  <div className="aura">
   <div className="flex-none">
    { !isElectron && ( 
-    <button className="btn btn-square btn-ghost">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path> </svg>
-    </button>)}
+    // <button className="">
+    //   {/* <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block h-5 w-5 stroke-current"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path> </svg> */}
+    //   Download the App
+    // </button>
+  
+    <button className="btn btn-xs sm:btn-sm md:btn-md bg-green-50 font-bold" title='Download the App'>
+       Download
+    </button>
+  )}
+  </div>
   </div>
 </div>
     </div>
