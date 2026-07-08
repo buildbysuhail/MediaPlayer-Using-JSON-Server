22e9bf5a63e3b30dcafd0fa283bd955c3d5c855f
Muhammed Suhail T <suhailthenoble@gmail.com>
2026-07-08 11:16:20 +0530

    Main scrollbar customized

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className="h-screen flex flex-col overflow-hidden">
       <Header/>
-      <div className="flex-1 min-h-0 overflow-y-auto">
+      <div className="flex-1 min-h-0 overflow-y-auto main-scroll">
       <Routes>
         <Route path='/' element={<Landing/>} />
         <Route path='/home' element={<Home/>} /> 
