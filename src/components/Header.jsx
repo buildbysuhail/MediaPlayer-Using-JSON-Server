0ea1ee6cd17c03a3ac9b6e43138a97b7b33234d3
Muhammed Suhail T <suhailthenoble@gmail.com>
2026-07-17 17:16:22 +0530

    V1.0.3

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const isElectron = window.suhailDesktop?.isElectron === true;
   console.log("isElectronnnnn", isElectron)
 
   const DOWNLOAD_URL =
-  "https://github.com/buildbysuhail/MediaPlayer-Using-JSON-Server/releases/download/v1.0.2/Media.Player.Setup.1.0.2.exe";
+  "https://github.com/buildbysuhail/MediaPlayer-Using-JSON-Server/releases/download/v1.0.3/Media.Player.Setup.1.0.3.exe";
 
   const handleDownloadExe = () => {
     window.open(DOWNLOAD_URL, "_blank");
